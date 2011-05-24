a651fa478de8ca3a90fc9555acddcecc931a998b
giu <public@giu.me>
2011-05-24 15:20:15 +0200

    Changed jQuery CDN from Google to jQuery. Now using the $ function works just fine

--- hackernews-threadify.js
+++ hackernews-threadify.js
@@ -1,16 +1,14 @@
 (function() {
   var s=document.createElement('script');
-  s.setAttribute('src','http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js');
+  s.setAttribute('src','http://code.jquery.com/jquery.min.js');
 
   if(typeof jQuery=='undefined') {
     document.getElementsByTagName('head')[0].appendChild(s);
   }
 
   window.setTimeout(function(){
-    var $$ = jQuery;
-
-    if($$("#hnthreadified").length===0){
-      $$("body").prepend("<input type='hidden' id='hnthreadified' />");
+    if($("#hnthreadified").length===0){
+      $("body").prepend("<input type='hidden' id='hnthreadified' />");
     }
     else{
       alert("This page has already been threadified!");
@@ -20,9 +18,9 @@
     var expand = "[+]";
     var reduce = "[-]";
 
-    $$(".default").each(function(i,e){
-      var $comhead = $$(".comhead", $$(this));
-      var $links = $$("a", $comhead);
+    $(".default").each(function(i,e){
+      var $comhead = $(".comhead", $(this));
+      var $links = $("a", $comhead);
       var delCount = 0;
 
       if($links.length > 0 && $links[0].innerHTML[0]===expand[0]){
@@ -34,24 +32,24 @@
         $comhead.prepend("<a class='collapselink' id='c_"+id+"' href='javascript:void(0)'>"+reduce+"</a> ");
       }
       else{ //comment has been deleted
-        $$("div", $$(this)).hide();
-        $$(this).prepend("<a class='collapselink' style='color:#828282' id='c_"+(++delCount)+"' href='javascript:void(0)'>"+reduce+"</a> ")
+        $("div", $(this)).hide();
+        $(this).prepend("<a class='collapselink' style='color:#828282' id='c_"+(++delCount)+"' href='javascript:void(0)'>"+reduce+"</a> ")
       }
       
     });
 
-    $$(".collapselink").live("click", function(){
-      var $t = $$(this);
+    $(".collapselink").live("click", function(){
+      var $t = $(this);
       var id = $t.attr("id").split(/_/)[1];
-      var $comment = $$(".comment", $t.closest("td"));//Get comment to which the collapse link is appended
-      var curIndentLevel = parseInt($$("td img", $t.closest("tr")).attr("width"));//Get the current comment's indentation level
+      var $comment = $(".comment", $t.closest("td"));//Get comment to which the collapse link is appended
+      var curIndentLevel = parseInt($("td img", $t.closest("tr")).attr("width"));//Get the current comment's indentation level
       var $com = $t.closest("table");
       var $nextCom = $com.closest("tr").next();//Comment following to the currently selected comment
       var changeToggleSign = true;
       var isExpand = $t.html() === expand;
 
       while($nextCom){
-        if($nextCom.length===0 || (parseInt($$("td img", $nextCom).attr("width")) <= curIndentLevel)){
+        if($nextCom.length===0 || (parseInt($("td img", $nextCom).attr("width")) <= curIndentLevel)){
           if(isExpand){
             $comment.show().next().show();
             $t.html(reduce);
@@ -69,8 +67,8 @@
               changeToggleSign=false;
               $comment.show().next().show();
             }
-            $$(".collapselink", $nextCom).html(reduce);
-            $$(".comment", $nextCom).show();
+            $(".collapselink", $nextCom).html(reduce);
+            $(".comment", $nextCom).show();
             $nextCom.show();
           }
           else{
