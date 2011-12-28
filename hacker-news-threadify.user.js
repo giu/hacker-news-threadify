d741e5413bb9997eb793ba730752c62a39b7bee7
giu <g@giu.me>
2011-12-28 10:31:39 +0100

    Chrome doesn't support the GM require tag. Added script to load JQuery file

--- hacker-news-threadify.user.js
+++ hacker-news-threadify.user.js
@@ -5,73 +5,92 @@
 // @include			http://news.ycombinator.com/item*
 // @include			http://news.ycombinator.com/threads*
 // @include			http://news.ycombinator.com/newcomments
-// @require			http://code.jquery.com/jquery.min.js
+// @version			1.1
 // ==/UserScript==
-var expand = "[+]";
-var reduce = "[-]";
 
-$(".default").each(function(i,e){
-  var $comhead = $(".comhead", $(this));
-  var $links = $("a", $comhead);
+//A thanks for this snippet goes to tghw:
+//http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
+function addJQuery(callback) {
+	var script = document.createElement("script");
+	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js");
+	script.addEventListener('load', function() {
+	var script = document.createElement("script");
+		script.textContent = "(" + callback.toString() + ")();";
+		document.body.appendChild(script);
+	}, false);
+	document.body.appendChild(script);
+}
 
-  if($links.length > 0 && $links[0].innerHTML[0]===expand[0]){
-    return;
-  }
+function threadify(){
 
-  if($links.length > 1){
-    var id = $links[$links.length-1].href.split(/=/)[1];
-    $comhead.prepend("<a class='collapselink' id='c_"+id+"' href='javascript:void(0)'>"+reduce+"</a> ");
-  }
-  else{//Comment has been deleted
-    $("div", $(this)).hide();
-    $(this).prepend("<a class='collapselink' style='color:#828282' id='c_"+i+"' href='javascript:void(0)'>"+reduce+"</a> ")
-  }
-  
-});
+	var expand = "[+]";
+	var reduce = "[-]";
 
-$(".collapselink").live("click", function(){
-  var $t = $(this);
-  var id = $t.attr("id").split(/_/)[1];
-  var $comment = $(".comment", $t.closest("td"));
-  var curIndentLevel = parseInt($("td img", $t.closest("tr")).attr("width"));
-  var $com = $t.closest("table");
-  var $nextCom = $com.closest("tr").next(); //Following comment
-  var changeToggleSign = true;
-  var isExpand = $t.html() === expand;
+	$(".default").each(function(i,e){
+		var $comhead = $(".comhead", $(this));
+		var $links = $("a", $comhead);
 
-  while($nextCom){
-    if($nextCom.length===0 || (parseInt($("td img", $nextCom).attr("width")) <= curIndentLevel)){
-      if(isExpand){
-        $comment.show().next().show();
-        $t.html(reduce);
-      }
-      else{
-        $comment.hide().next().hide();
-        $t.html(expand);
-      }
-      $nextCom = null;
-    }
-    else{
-      if(isExpand){
-        if(changeToggleSign){
-          $t.html(reduce);
-          changeToggleSign=false;
-          $comment.show().next().show();
-        }
-        $(".collapselink", $nextCom).html(reduce);
-        $(".comment", $nextCom).show();
-        $nextCom.show();
-      }
-      else{
-        if(changeToggleSign){
-          $t.html(reduce);
-          changeToggleSign=false;
-          $comment.hide().next().hide();
-        }
-        $nextCom.hide();
-      }
+		if($links.length > 0 && $links[0].innerHTML[0]===expand[0]){
+			return;
+		}
 
-      $nextCom = $nextCom.next();
-    }
-  }
-});
+		if($links.length > 1){
+			var id = $links[$links.length-1].href.split(/=/)[1];
+			$comhead.prepend("<a class='collapselink' id='c_"+id+"' href='javascript:void(0)'>"+reduce+"</a> ");
+		}
+		else{//Comment has been deleted
+			$("div", $(this)).hide();
+			$(this).prepend("<a class='collapselink' style='color:#828282' id='c_"+i+"' href='javascript:void(0)'>"+reduce+"</a> ")
+		}
+		
+	});
+
+	$(".collapselink").live("click", function(){
+		var $t = $(this);
+		var id = $t.attr("id").split(/_/)[1];
+		var $comment = $(".comment", $t.closest("td"));
+		var curIndentLevel = parseInt($("td img", $t.closest("tr")).attr("width"));
+		var $com = $t.closest("table");
+		var $nextCom = $com.closest("tr").next(); //Following comment
+		var changeToggleSign = true;
+		var isExpand = $t.html() === expand;
+
+		while($nextCom){
+			if($nextCom.length===0 || (parseInt($("td img", $nextCom).attr("width")) <= curIndentLevel)){
+				if(isExpand){
+					$comment.show().next().show();
+					$t.html(reduce);
+				}
+				else{
+					$comment.hide().next().hide();
+					$t.html(expand);
+				}
+				$nextCom = null;
+			}
+			else{
+				if(isExpand){
+					if(changeToggleSign){
+						$t.html(reduce);
+						changeToggleSign=false;
+						$comment.show().next().show();
+					}
+					$(".collapselink", $nextCom).html(reduce);
+					$(".comment", $nextCom).show();
+					$nextCom.show();
+				}
+				else{
+					if(changeToggleSign){
+						$t.html(reduce);
+						changeToggleSign=false;
+						$comment.hide().next().hide();
+					}
+					$nextCom.hide();
+				}
+
+				$nextCom = $nextCom.next();
+			}
+		}
+	});
+}
+
+addJQuery(threadify);
